306ce3b19035af1d5fa355d5e707e9e3e71c5666
Ed Willis <ewillis@jesande.com>
2015-03-22 17:46:43 -0400

    wip

--- search/all_66.js
+++ search/all_66.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['fakeserial',['FakeSerial',['../classfishtank__monitor__test_1_1FakeSerial.html',1,'fishtank_monitor_test']]],
-  ['fish_5ftank_5fdisplay',['fish_tank_display',['../namespacefish__tank__display.html',1,'']]],
   ['fishtank_5fmonitor',['fishtank_monitor',['../namespacefishtank__monitor.html',1,'']]],
   ['fishtank_5fmonitor_5ftest',['fishtank_monitor_test',['../namespacefishtank__monitor__test.html',1,'']]]
 ];
